e4e5ef8b3b50ea098d508eeddc3ff7b923bb285b
Lesley Lai <lesley@lesleylai.info>
2022-01-31 20:07:01 +0800

    Lesley Lai Ruby Day 2 self study

--- Chapter 2 - Ruby/lesley_lai_solutions.rb
+++ Chapter 2 - Ruby/lesley_lai_solutions.rb
@@ -38,3 +38,61 @@ while true
         break
     end
 end
+
+# Print the contents of an array of sixteen numbers, four numbers at a
+# time, using just each. Now, do the same with each_slice in Enumerable.
+arr = (0..15).to_a
+i = 0
+arr.each do
+    |v| if v % 4 == 3 then
+        print "%d\n" % v
+    else
+        print "%d, " % v
+    end
+    i += 1
+end
+
+arr.each_slice(4) { |v| p v }
+
+# The Tree class was interesting, but it did not allow you to specify a new
+# tree with a clean user interface. Let the initializer accept a nested
+# structure of hashes. You should be able to specify a tree like this:
+# {'grandpa' => { 'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {},
+# 'child 4' => {} } } }.
+class Tree
+    attr_accessor :children, :node_name
+    def initialize(hashes)
+        @node_name = hashes.keys[0]
+        @children = hashes.values[0].map do |k, v|
+             Tree.new({k => v})
+        end
+    end
+    
+    def visit_all(&block)
+        visit &block
+        children.each {|c| c.visit_all &block}
+    end
+
+    def visit(&block)
+        block.call self
+    end
+end
+
+ruby_tree = Tree.new( {'grandpa' => { 'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {}, 'child 4' => {} } } } )
+ruby_tree.visit_all { |node| puts node.node_name }
+
+# Write a simple grep that will print the lines of a file having any occurrences
+# of a phrase anywhere in that line. You will need to do a simple
+# regular expression match and read lines from a file. (This is surprisingly
+# simple in Ruby.) If you want, include line numbers.
+
+def grep(filename, str)
+    File.open(filename) do |file|
+        file.each_line
+            .each_with_index
+            .select { |line, _| line.include?(str) }
+            .each {|line, i| puts "%d %s" % [i, line]}
+    end
+end
+
+grep("lesley_lai_solutions.rb", "tree")
