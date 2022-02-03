093bafd77ef459ced1fa79f5cc1155bbbe99d55f
Lesley Lai <lesley@lesleylai.info>
2022-02-03 23:29:13 +0800

    Lesley Ruby day 3

--- Chapter 2 - Ruby/lesley_lai_solutions.rb
+++ Chapter 2 - Ruby/lesley_lai_solutions.rb
@@ -39,6 +39,8 @@ while true
     end
 end
 
+# Day 2
+
 # Print the contents of an array of sixteen numbers, four numbers at a
 # time, using just each. Now, do the same with each_slice in Enumerable.
 arr = (0..15).to_a
@@ -96,3 +98,61 @@ def grep(filename, str)
 end
 
 grep("lesley_lai_solutions.rb", "tree")
+
+# Day 3
+class CsvRow
+    def initialize(headers, row)
+        @headers = headers
+        @row = row
+    end
+
+    def method_missing(name, *args)
+        if(i = @headers.index(name.to_s))
+            @row[i]
+        else
+            raise ArgumentError.new("Method `#{name}` doesn't exist.")
+        end
+    end
+end
+
+module ActsAsCsv
+    def self.included(base)
+        base.extend ClassMethods
+    end
+
+    module ClassMethods
+        def acts_as_csv
+            include InstanceMethods
+        end
+    end
+
+    module InstanceMethods
+        def read
+            @csv_contents = []
+            filename = self.class.to_s.downcase + '.csv'
+            file = File.new(filename)
+            @headers = file.gets.chomp.split(', ')
+            file.each do |row|
+                @csv_contents << row.chomp.split(', ')
+            end
+        end
+
+        attr_accessor :headers, :csv_contents
+
+        def initialize
+            read
+        end
+
+        def each(&block)
+            @csv_contents.each { |row| block.call CsvRow.new(@headers, row) }
+        end
+    end
+end
+
+class RubyCsv
+    include ActsAsCsv
+    acts_as_csv
+end
+
+csv = RubyCsv.new
+csv.each {|row| puts row.one}
