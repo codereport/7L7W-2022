3e0dea328d407a6510e716affba8adeccf3c85c5
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2022-03-19 10:46:20 -0400

    Update code_from_book.pl

--- Chapter 4 - Prolog/code_from_book.pl
+++ Chapter 4 - Prolog/code_from_book.pl
@@ -60,12 +60,19 @@ coloring(Alabama, Mississippi, Georgia, Tennessee, Florida) :-
 % Day 2
 
 count(0, []).
-count(Count, [Head|Tail]) :- count(TailCount, Tail), Count is TailCount + 1.
+count(Count, [Head|Tail]) :- 
+   count(TailCount, Tail), 
+   Count is TailCount + 1.
 
 sum(0, []).
-sum(Total, [Head|Tail]) :- sum(Sum, Tail), Total is Head + Sum.
+sum(Total, [Head|Tail]) :- 
+   sum(Sum, Tail), 
+   Total is Head + Sum.
 
-average(Average, List) :- sum(Sum, List), count(Count, List), Average is Sum/Count.
+average(Average, List) :- 
+   sum(Sum, List), 
+   count(Count, List), 
+   Average is Sum/Count.
 
 % | ?- count(What, [1]).
 
