117842f1965c0f50953282ddb00be0d090e75d36
halloleo <git@halloleo.dot.hailmail.dot.net>
2022-03-22 10:27:40 +1100

    Prolog halloleo

---
+++ Chapter 4 - Prolog/halloleo_books_kb.pl
@@ -0,0 +1,16 @@
+% book('Title','Primary Author',StarRating,DateRead).
+book('The chimney sweeper-s boy','Vine, Barbara',star3.5,date2019_02_27).
+book('Algorithms to Live By: The Computer Science of Human Decisions','Christian, Brian',star4,date2020_02_08).
+book('Secret City: The Capital Files','Uhlmann, Chris',star3,date2020_02_28).
+book('Normal People','Rooney, Sally',star3,date2020_03_17).
+book('Coders: Who They Are, What They Think and How They Are Changing the World','Thompson, Clive',star3.5,date2020_04_21).
+book('The Mysterious Affair at Styles','Christie, Agatha',star3.5,date2020_04_18).
+book('Reverse Transmission','Ben O-Brien',star2,date2020_05).
+book('Faul zu sein ist harte Arbeit: Eine Ode an den Müßiggang','Liebmann, Martin',star1.5,date2020_06).
+book('The murder on the links','Christie, Agatha',star,date2020_08_11).
+book('Making movie magic : a lifetime creating special effects for James Bond, Harry Potter, Superman & more','Richardson, John',star,dateNone).
+book('Totensonntag: Kommissar Wallner 5','Föhr, Andreas',star3.5,date2020_12_06).
+book('Little Eyes: A Novel','Schweblin, Samanta',star3.5,date2021_09_08).
+book('Tschick','Herrndorf, Wolfgang',star3,date2021_07).
+book('Peter Holtz: Sein glückliches Leben erzählt von ihm selbst','Schulze, Ingo',star3,dateNone).
+book('Trost: Briefe an Max','Dorn, Thea',star4,date2022_01_02).
